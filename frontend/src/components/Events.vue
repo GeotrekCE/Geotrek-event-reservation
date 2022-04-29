ae9760dc9c44e62699f339e1f0016675bbb420b5
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-04-29 16:24:37 +0200

    Add publish and order column

--- frontend/src/components/Events.vue
+++ frontend/src/components/Events.vue
@@ -77,22 +77,25 @@
     >
       <template v-slot:item.id="{ item }">
         <router-link :to="{ name: 'event', params: { id: item.id }}">
-          <v-btn icon color="pink"  >
-            <v-icon>mdi-eye</v-icon>
+          <v-btn icon color="blue" >
+            <v-icon>mdi-text-box-multiple</v-icon>
           </v-btn>
         </router-link>
       </template>
       <template v-slot:item.remplissage="{ item }">
-
           <reservation-progress
             :reservation-nb="item.sum_participants"
             :participant-nb="item.participant_number"
             :attente-nb="item.sum_participants_liste_attente"
           >
           </reservation-progress>
-
       </template>
-
+          <template v-slot:item.published="{ item }">
+        <v-simple-checkbox
+          v-model="item.published"
+          disabled
+        ></v-simple-checkbox>
+      </template>
     </v-data-table>
   </div>
 </template>
@@ -121,14 +124,14 @@ export default {
         search_name: undefined,
       },
       headers: [
+        { text: 'Détail', value: 'id' },
         { text: 'Remplissage', value: 'remplissage' },
+        { text: 'Publié', value: 'published' },
         { text: 'Nom', value: 'name' },
         { text: 'Date début', value: 'begin_date' },
         { text: 'Date fin', value: 'end_date' },
         { text: 'Type', value: 'type.type' },
         { text: 'Massif', value: 'massif' },
-        { text: 'Attente', value: 'sum_participants_liste_attente' },
-        { text: 'id', value: 'id' },
       ],
       districts: [],
       eventtypes: []
