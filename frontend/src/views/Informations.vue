5bed9e73e3878bbb4273c40061de0f135c288f0d
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-06-13 22:28:45 +0200

    Page informations

---
+++ frontend/src/views/Informations.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <div v-html="markdownToHtml"></div>
+  </v-container>
+</template>
+
+<script>
+import { marked } from 'marked';
+
+export default {
+  data() {
+    return {
+      markdownToHtml: 'Aucun contenu'
+    };
+  },
+  computed: {
+  },
+  mounted() {
+    fetch('informations.md')
+      .then((response) => response.text())
+      .then((textString) => {
+        this.markdownToHtml = marked(textString);
+      }).catch((error) => {
+        this.markdownToHtml = 'Error when retrive informations.md'
+      });
+  }
+}
+
+</script>
