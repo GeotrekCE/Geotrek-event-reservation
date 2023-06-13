61118d4889e04cce4ad83397ded221ff00563962
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-06-13 10:00:41 +0200

    [front] Activation prime CSS

--- front-vite/src/main.ts
+++ front-vite/src/main.ts
@@ -3,15 +3,16 @@ import { createApp } from 'vue'
 import { pinia } from './plugins/pinia'
 import router from './router'
 
-import './index.css'
-
 /**
  * Prime Vue
  */
 import PrimeVue from 'primevue/config'
-// import 'primevue/resources/themes/lara-light-teal/theme.css'
+import 'primevue/resources/themes/lara-light-teal/theme.css'
 // import 'primevue/resources/primevue.min.css'
-// import 'primeicons/primeicons.css'   
+import 'primeicons/primeicons.css'
+
+import './index.css'
+
 
 import App from './App.vue'
 
