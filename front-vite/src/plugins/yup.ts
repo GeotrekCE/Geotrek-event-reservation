533cd9fdd9b1f6342fd62f30b73838bb7a0838b1
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-06-15 10:45:10 +0200

    [front] Traduction erreur de cast number

--- front-vite/src/plugins/yup.ts
+++ front-vite/src/plugins/yup.ts
@@ -3,12 +3,13 @@ import { setLocale } from 'yup';
 setLocale({
   mixed: {
     required: '${path} est un champ requis',
-    oneOf: '${path} doit correspond à une de ces valeurs : ${values}',  
+    oneOf: '${path} doit correspond à une de ces valeurs : ${values}',
+    notType: '${path} doit être de type `${type}`'
   },
   string: {
     email: '${path} doit être un mail valide'
   },
   number: {
-    min: '${path} doit être supérieur ou égal à 0'
+    min: '${path} doit être supérieur ou égal à 0',
   }
 })
