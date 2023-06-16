d85583d83ab07bd02e89e589a10e82b14c2379db
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-06-16 13:52:02 +0200

    [front] Refacto bouton LoginView

--- front-vite/src/views/LoginView.vue
+++ front-vite/src/views/LoginView.vue
@@ -24,7 +24,7 @@
         <div>
           <button
             type="submit"
-            class="flex w-full justify-center rounded-sm bg-sky-600 px-3 py-1.5 text-sm font-medium leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 disabled:bg-sky-200"
+            class="flex w-full justify-center rounded-sm bg-sky-600 px-3 py-1.5 text-sm font-medium leading-6 text-white shadow-sm hover:bg-sky-500 disabled:bg-sky-200"
             :disabled="loading"
           >
             Recevoir mon lien de connexion
