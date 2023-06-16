2d96cf1112e4cee6fe53c697b2816cd7f20a6461
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-06-16 13:51:34 +0200

    [front] Ajout lien de connexion sur page d'accueil

--- front-vite/src/views/HomeView.vue
+++ front-vite/src/views/HomeView.vue
@@ -7,10 +7,20 @@
     </div>
   </header>
   <main class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 px-4 py-6">
-    <section class="pb-12">
+    <section class="pb-12 mx-auto">
     	<p class="mb-8">
         Bienvenue sur l'outil de gestion des réservations d'événement du Parc National de Guadeloupe.
     	</p>
+
+      <p class="flex items-center">
+        <router-link
+          v-if="!authStore.isAuth"
+          to="/login"
+          class="rounded-sm bg-sky-600 px-3 py-1.5 text-sm font-medium leading-6 text-white shadow-sm hover:bg-sky-500 my-4 mx-auto"
+        >
+          Aller à la page de connexion
+        </router-link>
+      </p>
 
       <div v-if="loading">Loading...</div>
       <div v-html="markdownToHTML" v-else></div>
@@ -23,9 +33,12 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { marked } from 'marked';
+import { useAuthStore } from '@/stores/auth'
 
 const markdownToHTML = ref('')
 const loading = ref(false)
+
+const authStore = useAuthStore()
 
 onMounted(async() => {
   loading.value = true
