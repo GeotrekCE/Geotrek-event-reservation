adabbb9faa3580cddf94d6d683e4f22ea2ee3a42
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2023-06-29 12:00:02 +0200

    [backend] Fix confirmation réservation sur animation sans nombre de places

--- backend/core/routes.py
+++ backend/core/routes.py
@@ -372,6 +372,8 @@ def confirm_reservation():
     event = resa.event
     if not event.is_reservation_possible_for(resa.nb_participants):
         raise EventIsFull
+    elif not event.capacity:
+        resa.liste_attente = False
     elif event.sum_participants + resa.nb_participants <= event.capacity:
         resa.liste_attente = False
     else:
