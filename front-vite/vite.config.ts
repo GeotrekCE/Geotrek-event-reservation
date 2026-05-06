e96219ad165a770927ba52f03f53b5a05b4c246a
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2026-05-06 09:47:02 +0200

    Vite : Ajout d'un parametre specifiant le nom du sous dossier

--- front-vite/vite.config.ts
+++ front-vite/vite.config.ts
@@ -1,18 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+ 
 
-export default defineConfig({
-  plugins: [
-    vue(), 
-    tailwindcss()
-  ],
-    resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(), 
+      tailwindcss()
+    ],
+      resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+      base: env.VITE_BASE_PATH || '/', // fallback sécurité
     }
-  }
 })
